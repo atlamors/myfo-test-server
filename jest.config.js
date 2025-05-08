023267de1aa3485d547b56f653f41b55ce813e7b
Andrew Nelson <hello@andrewnelson.net>
2025-05-07 20:31:03 -0700

    Refactor jest.config.js for improved formatting and consistency

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,24 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/**/*.test.ts',
-    '!src/types/**/*.ts'
-  ],
-  coverageDirectory: 'coverage',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  moduleNameMapper: {
-    '^@middleware/(.*)$': '<rootDir>/src/middleware/$1',
-    '^@routes/(.*)$': '<rootDir>/src/routes/$1',
-    '^@config$': '<rootDir>/src/config.ts',
-    '^@types/(.*)$': '<rootDir>/src/types/$1',
-    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
-    '^@/(.*)$': '<rootDir>/src/$1'
-  }
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest'
+    },
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/*.d.ts',
+        '!src/**/*.test.ts',
+        '!src/types/**/*.ts'
+    ],
+    coverageDirectory: 'coverage',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    moduleNameMapper: {
+        '^@middleware/(.*)$': '<rootDir>/src/middleware/$1',
+        '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+        '^@types/(.*)$': '<rootDir>/src/types/$1',
+        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^@/(.*)$': '<rootDir>/src/$1'
+    }
 }; 
